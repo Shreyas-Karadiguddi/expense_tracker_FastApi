778eb21bd36d1a9b5d02cd2538e78c8a79cd168a
Shreyas <shreyasskkaradiguddi@gmail.com>
2024-07-07 03:03:29 +0530

    new changes

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const Login = () => {
+// fonsa bhi function
+
+  
+    return (
+        <><div style={{marginLeft:'400px', marginRight:'400px', marginTop:'50px'}}>
+        <Card style={{textAlignLast:'center'}}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+    </Card></div>
+        
+        </>
+    )
+}
+
+export default Login
