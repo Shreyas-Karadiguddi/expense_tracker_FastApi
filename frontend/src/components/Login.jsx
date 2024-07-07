ab05de3a3dd7c6d9845ed40af29c611eef45fd9f
tejas <tejas.b@analytic-edge.com>
2024-07-08 01:26:30 +0530

    Login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,24 +1,61 @@
-import React from "react";
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Login = () => {
-// fonsa bhi function
+  // fonsa bhi function
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [action, setAction] = useState(false);
 
-  
-    return (
-        <><div style={{marginLeft:'400px', marginRight:'400px', marginTop:'50px'}}>
-        <Card style={{textAlignLast:'center'}}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  const handleOnChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-    </Card></div>
-        
-        </>
-    )
-}
+  const handleOnClick = (e) => {
+    setAction(!action);
+    temp(action);
+  };
 
-export default Login
+  const temp = (ac) => {
+    alert(ac);
+  };
+
+  useEffect(() => {
+    console.log("Yes", action);
+  }, [action]);
+
+  return (
+    <>
+      <div
+        style={{
+          marginLeft: "400px",
+          marginRight: "400px",
+          marginTop: "50px",
+        }}
+      >
+        <TextField
+          label="Username"
+          variant={action == false ? "standard" : "outlined"}
+          value={username}
+          onChange={handleOnChange}
+        />
+        <br />
+        <TextField
+          label="Password"
+          variant={action == false ? "standard" : "outlined"}
+          value={password}
+          type="password"
+        />
+        <br />
+        <Button variant="contained" onClick={handleOnClick}>
+          SignUp
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Login;
